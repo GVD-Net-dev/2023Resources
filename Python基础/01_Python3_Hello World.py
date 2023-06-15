0e093c56c2e42535ce87aa4af890d29247b8846b
Dog <46147274+SlightQTMQ@users.noreply.github.com>
2023-06-15 15:18:10 +0800

    Add files via upload

---
+++ Python基础/01_Python3_Hello World.py
@@ -0,0 +1,43 @@
+
+########### 一. 注释
+
+user = "zqy"  # 在代码后面加注释
+print("一. 注释")
+print(user)
+
+########### 二. 几何运算
+print("二. 几何运算")
+print(10+30)
+print(40-10)
+print(50*5)
+print(16/4)
+print(25%2)
+print(5**3)
+
+########### 三. 简写
+print("三. 简写")
+counter = 0
+counter +=10
+print(counter)
+
+# 字符串拼接也是可以这样子写的
+str = "part 1 of message;"
+str += "part 2 of message;"
+print(str)
+
+
+########### 四. 取余符号
+print("四. 取余符号")
+
+zero = 8 % 4
+print(zero)
+nonzero = 12 % 5
+print(nonzero)
+
+
+########### 五. 字符串拼接
+print("五. 字符串拼接")
+first = "hello"
+second ="world"
+result = first + second
+print(result)
