7b810fce7b6f015f5428effe4c00973db61b2391
Dog <46147274+SlightQTMQ@users.noreply.github.com>
2023-06-23 18:33:03 +0800

    Delete Python基础 directory

--- Python基础/Chapter2—— 变量和数据类型/03_复数Complex.py
+++
@@ -1,6 +0,0 @@
-Num1 = 2+3j  # 第一个复数 2+3i
-Num2 = 3+4j  # 第二个复数 3+4j
-print('Num1+Num2=',Num1+Num2) # 复数加法
-print('Num1-Num2=',Num1-Num2) # 复数减法
-# Num1+Num2= (5+7j)
-# Num1-Num2= (-1-1j)
--- Python基础/chapter1—Python输入和输出/02_input()函数的使用.py
+++
@@ -1,4 +0,0 @@
-name = input("请输入字符:") # 输入字母或数字，不能输入汉字
-print(name," 的ASCII码为: ",ord(name))
-# 请输入字符:A
-# A  的ASCII码为:  65
