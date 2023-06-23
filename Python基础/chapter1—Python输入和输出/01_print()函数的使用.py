d64339738348ea39d52df9c00d4c2f1a0bb45eea
Dog <46147274+SlightQTMQ@users.noreply.github.com>
2023-06-23 18:27:34 +0800

    Add files via upload

---
+++ Python基础/chapter1—Python输入和输出/01_print()函数的使用.py
@@ -0,0 +1,21 @@
+a = 100 # 变量a，值为100
+b = 5 # 变量b，值为5
+
+# print 练习
+print(9) # 输出数字9
+print(a) # 输出变量a的值为100
+print(a*b) #输出a*b的结果为500
+print("hello world") # 输出字符串
+
+# 不换行==> 将要输出的内容使用英文半角的逗号分隔
+
+a =100
+b = 5
+print(a,b,'hello world')
+
+# 使用ASCII码输出
+print('a') # 输出字符a
+print(chr(97)) # 输出字符a（字符a的ASCII码是97）
+
+print('A') # 输出字符A
+print(chr(65)) # 输出字符A（字符A的ASCII码是65）
